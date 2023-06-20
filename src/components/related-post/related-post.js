731159174aa8469513a14fddc9b76d0c0ef5af53
Hossein Karami <hi@hosseinkarami.com>
2023-06-20 15:47:09 +0300

    feat: related post

---
+++ src/components/related-post/related-post.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { GatsbyImage } from 'gatsby-plugin-image';
+import ContainerBox from '../container-box/container-box';
+
+const RelatedPostsSection = ({ relatedPosts }) => (
+  <ContainerBox className="c-section--related">
+    <div className="c-related">
+      <div className="c-blog__list">
+        {relatedPosts.map(({ node }) => (
+          <div className="c-blog__item" key={node.id}>
+            <div className="c-blog-post__image">
+              <Link to={node.uri} className="c-link">
+                <GatsbyImage image={node.featuredImage.node.localFile.childImageSharp.gatsbyImageData} alt={node.featuredImage.node.altText} />
+              </Link>
+            </div>
+            <div className="c-blog-post__category">
+              <Link to="/" className="c-link c-link--category">Blog</Link>
+            </div>
+            <h3 className="c-blog-post__title">
+              <Link to={node.uri} className="c-link c-link--blog">{node.title}</Link>
+            </h3>
+            <div className="c-blog-post__cta">
+              <Link to={node.uri} className="c-btn--secondary">
+                Read More
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" fill="none"><circle cx="12" cy="12" r="12" fill="#59CC51"/><path fill="#fff" d="M17.53 12.53a.75.75 0 0 0 0-1.06l-4.773-4.773a.75.75 0 0 0-1.06 1.06L15.939 12l-4.242 4.243a.75.75 0 0 0 1.06 1.06l4.773-4.773ZM6 12.75h11v-1.5H6v1.5Z"/></svg>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </ContainerBox>
+);
+
+export default RelatedPostsSection;
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,50 +5,56 @@ import Seo from "../components/seo/seo"
 import Layout from "../components/layout/layout"
 import ShareButton from "../components/share-btn/share-btn"
 import CtaPost from "../components/cta-post/cta-post"
+import RelatedPostsSection from "../components/related-post/related-post"
+import ContainerBox from "../components/container-box/container-box"
 
 const BlogPostTemplate = ({ data }) => {
-  const post = data.wpPost;
+  const post = data.singlePost;
+  const recentPosts = data.recentPosts.edges;
   const currentUrl = typeof window !== 'undefined' ? window.location.href : '';
 
   console.log(post);
   return (
     <Layout>
-      <article className="c-article">
-        <header className="c-article__header">
-          <div className="c-article__header-wrapper">
-            <div className="c-article__meta">
-              <div className="c-article__category">
-                <Link to="/" className="c-link c-link--category">Blog</Link>
-              </div>
-              <h1 className="c-article__title">{post.title}</h1>
-              <div className="c-article__author">
-                <div className="c-article__author-avatar">
-                  <img src={post.author.node.avatar.url} alt={post.author.node.firstName} />
+      <ContainerBox className="c-section--article">
+        <article className="c-article">
+          <header className="c-article__header">
+            <div className="c-article__header-wrapper">
+              <div className="c-article__meta">
+                <div className="c-article__category">
+                  <Link to="/" className="c-link c-link--category">Blog</Link>
                 </div>
-                <div className="c-article__author-info">
-                  <div className="c-article__author-name">
-                    {post.author.node.firstName} {post.author.node.lastName}
+                <h1 className="c-article__title">{post.title}</h1>
+                <div className="c-article__author">
+                  <div className="c-article__author-avatar">
+                    <img src={post.author.node.avatar.url} alt={post.author.node.firstName} />
                   </div>
-                  <div className="c-article__author-date">
-                    {post.date}
+                  <div className="c-article__author-info">
+                    <div className="c-article__author-name">
+                      {post.author.node.firstName} {post.author.node.lastName}
+                    </div>
+                    <div className="c-article__author-date">
+                      {post.date}
+                    </div>
                   </div>
                 </div>
               </div>
+                <div className="c-article__image">
+                  <GatsbyImage image={post.featuredImage.node.localFile.childImageSharp.gatsbyImageData} alt={post.featuredImage.node.altText} />
+                </div>
             </div>
-              <div className="c-article__image">
-                <GatsbyImage image={post.featuredImage.node.localFile.childImageSharp.gatsbyImageData} alt={post.featuredImage.node.altText} />
-              </div>
-          </div>
-        </header>
+          </header>
 
-        <div className="c-article__content">
-          <div className="c-article__content-wrapper s-content" dangerouslySetInnerHTML={{__html:post.content}}></div>
-          <div className="c-article__share">
-            <ShareButton postUrl={currentUrl} postTitle={post.title} />
+          <div className="c-article__content">
+            <div className="c-article__content-wrapper s-content" dangerouslySetInnerHTML={{__html:post.content}}></div>
+            <div className="c-article__share">
+              <ShareButton postUrl={currentUrl} postTitle={post.title} />
+            </div>
           </div>
-        </div>
-        <CtaPost />
-      </article>
+          <CtaPost />
+        </article>
+      </ContainerBox>
+      <RelatedPostsSection relatedPosts={recentPosts} />
     </Layout>
   )
 }
@@ -56,7 +62,7 @@ const BlogPostTemplate = ({ data }) => {
 export default BlogPostTemplate
 
 export function Head({ data }) {
-  const post = data.wpPost;
+  const post = data.singlePost;
   return (
     <>
       <Seo title={post.title} description={post.excerpt} featuredImage={post.featuredImage.node.localFile.url} />
@@ -66,7 +72,7 @@ export function Head({ data }) {
 
 export const pageQuery = graphql`
   query($id: String!) {
-    wpPost(id: {eq: $id}) {
+    singlePost: wpPost(id: {eq: $id}) {
       databaseId
       title
       uri
@@ -96,6 +102,31 @@ export const pageQuery = graphql`
           }
           firstName
           lastName
+        }
+      }
+    }
+    recentPosts: allWpPost(
+      limit: 2
+      filter: { id: { ne: $id } }
+    ) {
+      edges {
+        node {
+          id
+          title
+          slug
+          uri
+          excerpt
+          date(formatString: "MMMM DD, YYYY")
+          featuredImage {
+            node {
+              localFile {
+                childImageSharp {
+                  gatsbyImageData
+                }
+              }
+              altText
+            }
+          }
         }
       }
     }
