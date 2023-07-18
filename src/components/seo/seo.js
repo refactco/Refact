717638580846b594b04a66aa7646995516f16a3c
Hossein Karami <hi@hosseinkarami.com>
2023-07-18 10:48:42 +0300

    fix: SEO

--- src/components/seo/seo.js
+++ src/components/seo/seo.js
@@ -25,6 +25,16 @@ const Seo = ({ title, description, pathname, featuredImage, children }) => {
       <meta property="og:image" content={seo.image} />
       <meta property="og:image:width" content="2400" />
       <meta property="og:image:height" content="1260" />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:site_name" content="Refact" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:url" content={seo.url} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
       {children}
     </>
   )
