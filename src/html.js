2fefa97f8c63b9f430ff8e9bc6fdded693bec461
Hossein Karami <hi@hosseinkarami.com>
2023-07-19 09:20:19 +0300

    fix: code

--- src/html.js
+++ src/html.js
@@ -17,26 +17,6 @@ export default function HTML(props) {
             `,
           }}
         />
-        <script dangerouslySetInnerHTML={{
-          __html: `
-          var _ss = _ss || [];
-          _ss.push(['_setDomain', 'https://koi-3RZSEK5NRO.marketingautomation.services/net']);
-          _ss.push(['_setAccount', 'KOI-1FZ71YL3Y3J6HU']);
-          _ss.push(['_trackPageView']);
-          window._pa = window._pa || {};
-          // _pa.orderId = "myOrderId"; // OPTIONAL: attach unique conversion identifier to conversions
-          // _pa.revenue = "19.99"; // OPTIONAL: attach dynamic purchase values to conversions
-          // _pa.productId = "myProductId"; // OPTIONAL: Include product ID for use with dynamic ads
-      (function() {
-          var ss = document.createElement('script');
-          ss.type = 'text/javascript'; ss.async = true;
-          ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'koi-3RZSEK5NRO.marketingautomation.services/client/ss.js?ver=2.4.0';
-          var scr = document.getElementsByTagName('script')[0];
-          scr.parentNode.insertBefore(ss, scr);
-      })();
-          `,
-        }}
-        />
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
