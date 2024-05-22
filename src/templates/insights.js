93f8a78dac97ed9cf04e2a0223a2dc799eb90749
Hossein Karami <hi@hosseinkarami.com>
2024-05-22 11:19:30 +0300

    feat: framer motion
    Co-Authored-By: Hamidreza Amini <hramini72@gmail.com>

--- src/templates/insights.js
+++ src/templates/insights.js
@@ -1,4 +1,5 @@
 import { Link, graphql, navigate } from 'gatsby';
+import { AnimatePresence, motion } from 'framer-motion';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import React from 'react';
 import ResponsivePagination from 'react-responsive-pagination';
@@ -15,146 +16,38 @@ const InsightPage = (props) => {
 
   return (
     <Layout>
-      {/* <ContainerBox className="c-section--page-header t-light">
-        <div className="c-page-header">
-          <h1 className="c-page-header__title">
-            Insights
-          </h1>
-        </div>
-      </ContainerBox> */}
-      <ContainerBox className="c-section--blog">
-        <div className="c-blog-featured">
-          <div className="c-blog-featured__wrap">
-            <div className="c-blog-post__badge">Featured post</div>
-            <div className="c-blog-post__category">
-              {firstPost.node.tags.nodes.map((tag) => (
-                <Link
-                  to={tag.link}
-                  className="c-link c-link--category"
-                  key={tag.id}
-                >
-                  {tag.name}
-                </Link>
-              ))}
-              {/* <Link to={firstPost.node.tags.nodes[0].link} className="c-link c-link--category">{firstPost.node.tags.nodes[0].name}</Link> */}
-            </div>
-            <h2 className="c-blog-featured__title">
-              <Link to={firstPost.node.uri} className="c-link c-link--blog">
-                {firstPost.node.title}
-              </Link>
-            </h2>
-            {firstPost.node.excerpt && (
-              <div
-                className="c-blog-featured__excerpt"
-                dangerouslySetInnerHTML={{
-                  __html: data.allWpPost.edges[0].node.excerpt,
-                }}
-              ></div>
-            )}
-            <div className="c-blog-featured__cta">
-              <Link to={firstPost.node.uri} className="c-btn--secondary">
-                Read More
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                >
-                  <circle cx="12" cy="12" r="12" fill="#59CC51" />
-                  <path
-                    fill="#fff"
-                    d="M17.53 12.53a.75.75 0 0 0 0-1.06l-4.773-4.773a.75.75 0 0 0-1.06 1.06L15.939 12l-4.242 4.243a.75.75 0 0 0 1.06 1.06l4.773-4.773ZM6 12.75h11v-1.5H6v1.5Z"
-                  />
-                </svg>
-              </Link>
-            </div>
-          </div>
-          <div className="c-blog-featured__image c-blog-post__image">
-            <Link to={firstPost.node.uri} className="c-link">
-              {firstPost.node.featuredImage ? (
-                <GatsbyImage
-                  image={
-                    firstPost.node.featuredImage.node.localFile.childImageSharp
-                      .gatsbyImageData
-                  }
-                  alt={firstPost.node.featuredImage.node.altText}
-                />
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="711"
-                  height="447"
-                  fill="none"
-                  viewBox="0 0 711 447"
-                >
-                  <path fill="#E5F7E3" d="M0 0h711v447H0z" />
-                </svg>
-              )}
-            </Link>
-          </div>
-        </div>
-        <div className="c-blog-nav">
-          <div className="c-blog-nav__title">Topics</div>
-          <div className="c-blog-nav__wrap is-insight">
-            <swiper-container
-              space-between={8}
-              slides-per-view={'auto'}
-              css-mode={false}
-              navigation={false}
-              allow-touch-move={true}
-            >
-              {topicItems.map((topic) => (
-                <swiper-slide key={topic.id}>
-                  <div className="item">
-                    <Link to={topic.link}>{topic.name}</Link>
-                  </div>
-                </swiper-slide>
-              ))}
-            </swiper-container>
-          </div>
-        </div>
-        <div className="c-blog-posts">
-          <div className="c-blog__list">
-            {posts.map(({ node }) => (
-              <div className="c-blog__item" key={node.id}>
-                <div className="c-blog-post__image">
-                  <Link to={node.uri} className="c-link">
-                    <GatsbyImage
-                      image={
-                        node.featuredImage.node.localFile.childImageSharp
-                          .gatsbyImageData
-                      }
-                      alt={node.featuredImage.node.altText}
-                    />
-                  </Link>
-                </div>
+      <AnimatePresence>
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -50, opacity: 0 }}
+        >
+          <ContainerBox className="c-section--blog">
+            <div className="c-blog-featured">
+              <div className="c-blog-featured__wrap">
+                <div className="c-blog-post__badge">Featured post</div>
                 <div className="c-blog-post__category">
-                  {node.tags.nodes.map((tag) => (
-                    <Link
-                      to={tag.link}
-                      className="c-link c-link--category"
-                      key={tag.id}
-                    >
+                  {firstPost.node.tags.nodes.map((tag) => (
+                    <Link to={tag.link} className="c-link c-link--category" key={tag.id}>
                       {tag.name}
                     </Link>
                   ))}
                 </div>
-
-                <h3 className="c-blog-post__title">
-                  <Link to={node.uri} className="c-link c-link--blog">
-                    {node.title}
+                <h2 className="c-blog-featured__title">
+                  <Link to={firstPost.node.uri} className="c-link c-link--blog">
+                    {firstPost.node.title}
                   </Link>
-                </h3>
-
-                <div className="c-blog-post__cta">
-                  <Link to={node.uri} className="c-btn--secondary">
+                </h2>
+                {firstPost.node.excerpt && (
+                  <div
+                    className="c-blog-featured__excerpt"
+                    dangerouslySetInnerHTML={{ __html: data.allWpPost.edges[0].node.excerpt }}
+                  ></div>
+                )}
+                <div className="c-blog-featured__cta">
+                  <Link to={firstPost.node.uri} className="c-btn--secondary">
                     Read More
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                    >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" fill="none">
                       <circle cx="12" cy="12" r="12" fill="#59CC51" />
                       <path
                         fill="#fff"
@@ -164,20 +57,88 @@ const InsightPage = (props) => {
                   </Link>
                 </div>
               </div>
-            ))}
-          </div>
-          <div className="c-blog-posts__pagination">
-            <ResponsivePagination
-              current={page}
-              total={totalPages}
-              maxWidth={200}
-              onPageChange={(changedPage) => {
-                navigate(`/insights/${changedPage}`);
-              }}
-            />
-          </div>
-        </div>
-      </ContainerBox>
+              <div className="c-blog-featured__image c-blog-post__image">
+                <Link to={firstPost.node.uri} className="c-link">
+                  {firstPost.node.featuredImage ? (
+                    <GatsbyImage
+                      image={firstPost.node.featuredImage.node.localFile.childImageSharp.gatsbyImageData}
+                      alt={firstPost.node.featuredImage.node.altText}
+                    />
+                  ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" width="711" height="447" fill="none" viewBox="0 0 711 447">
+                      <path fill="#E5F7E3" d="M0 0h711v447H0z" />
+                    </svg>
+                  )}
+                </Link>
+              </div>
+            </div>
+            <div className="c-blog-nav">
+              <div className="c-blog-nav__title">Topics</div>
+              <div className="c-blog-nav__wrap is-insight">
+                <swiper-container space-between={8} slides-per-view={'auto'} css-mode={false} navigation={false} allow-touch-move={true}>
+                  {topicItems.map((topic) => (
+                    <swiper-slide key={topic.id}>
+                      <div className="item">
+                        <Link to={topic.link}>{topic.name}</Link>
+                      </div>
+                    </swiper-slide>
+                  ))}
+                </swiper-container>
+              </div>
+            </div>
+            <div className="c-blog-posts">
+              <div className="c-blog__list">
+                {posts.map(({ node }) => (
+                  <div className="c-blog__item" key={node.id}>
+                    <div className="c-blog-post__image">
+                      <Link to={node.uri} className="c-link">
+                        <GatsbyImage
+                          image={node.featuredImage.node.localFile.childImageSharp.gatsbyImageData}
+                          alt={node.featuredImage.node.altText}
+                        />
+                      </Link>
+                    </div>
+                    <div className="c-blog-post__category">
+                      {node.tags.nodes.map((tag) => (
+                        <Link to={tag.link} className="c-link c-link--category" key={tag.id}>
+                          {tag.name}
+                        </Link>
+                      ))}
+                    </div>
+                    <h3 className="c-blog-post__title">
+                      <Link to={node.uri} className="c-link c-link--blog">
+                        {node.title}
+                      </Link>
+                    </h3>
+                    <div className="c-blog-post__cta">
+                      <Link to={node.uri} className="c-btn--secondary">
+                        Read More
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" fill="none">
+                          <circle cx="12" cy="12" r="12" fill="#59CC51" />
+                          <path
+                            fill="#fff"
+                            d="M17.53 12.53a.75.75 0 0 0 0-1.06l-4.773-4.773a.75.75 0 0 0-1.06 1.06L15.939 12l-4.242 4.243a.75.75 0 0 0 1.06 1.06l4.773-4.773ZM6 12.75h11v-1.5H6v1.5Z"
+                          />
+                        </svg>
+                      </Link>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="c-blog-posts__pagination">
+                <ResponsivePagination
+                  current={page}
+                  total={totalPages}
+                  maxWidth={200}
+                  onPageChange={(changedPage) => {
+                    navigate(`/insights/${changedPage}`);
+                  }}
+                />
+              </div>
+            </div>
+          </ContainerBox>
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 };
