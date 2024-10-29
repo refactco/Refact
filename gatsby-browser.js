54f609427958a0858f75838f478e3fbbe6b9410a
Hossein Karami <hi@hosseinkarami.com>
2024-10-29 16:52:58 +0300

    Fix: branch name check

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,18 +1,5 @@
-import React from 'react';
 import { setUTMCookies } from './src/utils/utm';
 
 export const onClientEntry = () => {
   setUTMCookies();
-};
-
-
-export const onRenderBody = ({ setPostBodyComponents }) => {
-  setPostBodyComponents([
-    <script
-      key="branch-context"
-      dangerouslySetInnerHTML={{
-        __html: `window.branch = "${process.env.BRANCH_NAME || 'main'}";`,
-      }}
-    />,
-  ]);
 };
